4c662650156d3fbf080b48e7815eec0b4d4dd9e1
x-pulse <kuchurov03@mail.ru>
2019-11-19 10:37:18 +0000

    Publish

---
+++ README.md
@@ -0,0 +1,3 @@
+# words
+
+Just words game, like keys, but words, check it here https://xeval.dev/w/words
---
+++ index.js
@@ -0,0 +1,36 @@
+const wordtoguess = document.querySelector('#wordtoguess');
+const placetoguess = document.querySelector('#placetoguess');
+const points_html = document.querySelector('#points');
+const reload = document.querySelector('#reload')
+
+let points = 0;
+let word = '';
+
+function getWord() {
+
+    let word_nr = Math.floor(Math.random() * words.length)
+    word = words[word_nr];
+    console.log('И выпадает: ' + word)
+    wordtoguess.innerHTML = word;
+
+}
+
+window.onload = getWord()
+
+placetoguess.oninput = () => {
+    let guessedvalue = placetoguess.value;
+    let keyword = wordtoguess.innerHTML;
+
+    placetoguess.setAttribute("maxlength", keyword.length);
+
+    if (guessedvalue == keyword) {
+        points++;
+        points_html.innerHTML = points;
+        getWord()
+        placetoguess.value = ''
+    }
+}
+
+reload.addEventListener('click', () => {
+    getWord()
+})
